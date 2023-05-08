efa884b968c28c227006f103db9cf7fff6b21ad8
Juan Cruz Gonzalez <juancruzg30@gmail.com>
2023-05-08 17:33:51 -0300

    agregando productoria,contatoria y sumatoria.

--- practica8.hs
+++ practica8.hs
@@ -14,6 +14,23 @@ existe xs ys f = or [f x | x <- xs]
 paraTodo :: [Int] -> [a] -> (Int -> [a] -> Bool)-> Bool
 paraTodo xs ys f = and [f x | x <- xs]
 
-f :: [a] -> Bool
+--f :: [a] -> Bool
+
+existeUnNMayA0 :: [Int] -> Bool
+existeUnNMayA0 xs = or [x > 10 | x <- xs]
+
+todosSonPos :: [Int] -> Bool
+todosSonPos xs = and [x > 0 | x <- xs]
+
+sumatoriaPares :: [Int] -> Int 
+sumatoriaPares [] = 0 -- elemento neutro para la operacion suma
+sumatoriaPares (x:xs) = sum [x | x <- xs, even x] 
+
+productoriaDivisoresDe3 :: [Int] -> Int
+productoriaDivisoresDe3 [] = 1 -- elemento neutro para la operacion producto
+productoriaDivisoresDe3 xs = product [x | x <- xs, mod x 3 == 0]
 
 
+contatoriaImparesMayA15 :: [Int] -> Int
+contatoriaImparesMayA15 [] = 0 -- elemento neutro para la operacion contatoria
+contatoriaImparesMayA15 xs = length [x | x <- xs, odd x && x > 15]
